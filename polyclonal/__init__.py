a52405d45622d73d1d5d47bb640b7d8b87a3d118
jbloom <jbloom@fredhutch.org>
2021-09-29 11:31:23 -0700

    import `Polyclonal` into main package namespace

--- polyclonal/__init__.py
+++ polyclonal/__init__.py
@@ -3,9 +3,19 @@
 polyclonal
 ================================
 
+Package for modeling mutational escape from polyclonal antibodies using
+deep mutational scanning experiments.
+
+Importing this package imports the following objects
+into the package namespace:
+
+ - :py:mod:`polyclonal.polyclonal.Polyclonal`
+
 """
 
 __author__ = '`the Bloom lab <https://research.fhcrc.org/bloom/en.html>`_'
 __email__ = 'jbloom@fredhutch.org'
 __version__ = '0.1'
 __url__ = 'https://github.com/jbloomlab/polyclonal'
+
+from polyclonal.polyclonal import Polyclonal  # noqa: F401
