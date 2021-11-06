d951ac6534c76a178e05ca1085017d1edaed3f59
jbloom <jbloom@fredhutch.org>
2021-11-05 20:37:54 -0700

    added `utils.MutParser`

--- polyclonal/utils.py
+++ polyclonal/utils.py
@@ -10,6 +10,58 @@ Miscellaneous utility functions.
 
 import re
 
+from binarymap.binarymap import AAS_NOSTOP
+
+
+class MutationParser:
+    """Parse mutation strings like 'A5G'.
+
+    Parameters
+    ----------
+    alphabet : array-like
+        Valid single-character letters in alphabet.
+
+    Example
+    -------
+    >>> mutparser = MutationParser(AAS_NOSTOP)
+    >>> mutparser.parse_mut('A5G')
+    ('A', 5, 'G')
+
+    """
+
+    def __init__(self, alphabet):
+        """See main class docstring."""
+        chars = []
+        for char in alphabet:
+            if char.isalpha():
+                chars.append(char)
+            elif char == '*':
+                chars.append(r'\*')
+            else:
+                raise ValueError(f"invalid alphabet character: {char}")
+        chars = '|'.join(chars)
+        self._mutation_regex = re.compile(rf"(?P<wt>{chars})"
+                                          rf"(?P<site>\d+)"
+                                          rf"(?P<mut>{chars})")
+
+    def parse_mut(self, mutation):
+        """tuple: `(wildtype, site, mutation)`."""
+        m = self._mutation_regex.fullmatch(mutation)
+        if not m:
+            raise ValueError(f"invalid mutation {mutation}")
+        else:
+            return (m.group('wt'), int(m.group('site')), m.group('mut'))
+
+
+def site_level_variants(df,
+                        *,
+                        original_alphabet=AAS_NOSTOP,
+                        wt_char='w',
+                        mut_char='m',
+                        ):
+    """Re-define variants simply in terms of which sites are mutated."""
+    raise NotImplementedError
+
 
 def shift_mut_site(mut_str, shift):
     """Shift site in string of mutations.
