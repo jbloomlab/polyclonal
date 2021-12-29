5ee4bf5a2044ceb56dc8adf1fb7cf0e643cc199d
Erick Matsen <ematsen@gmail.com>
2021-12-29 15:11:41 -0800

    optimizing full loss

--- polyclonal/fit.py
+++ polyclonal/fit.py
@@ -21,12 +21,29 @@ def trivial_prox(params, t):
     return params
 
 
-def prox_grad_of_polyclonal(poly_abs, delta):
-    exact_bv_sparse = loss.bv_sparse_of_bmap(poly_abs._binarymaps)
+def prox_grad_of_polyclonal(
+    poly_abs,
+    loss_delta=0.1,
+    reg_escape_weight=0.01,
+    reg_escape_delta=0.1,
+    reg_spread_weight=0.25,
+):
+    bv_sparse = loss.bv_sparse_of_bmap(poly_abs._binarymaps)
+    (matrix_to_mean, coeff_positions) = loss.spread_matrices_of_polyclonal(poly_abs)
     loss_grad = jax.grad(loss.loss, 0)
+    args = [
+        poly_abs,
+        bv_sparse,
+        loss_delta,
+        reg_escape_weight,
+        reg_escape_delta,
+        reg_spread_weight,
+        matrix_to_mean,
+        coeff_positions,
+    ]
     return optimization.AccProxGrad(
-        lambda p: loss.loss(p, poly_abs, exact_bv_sparse, delta),
-        lambda p: loss_grad(p, poly_abs, exact_bv_sparse, delta),
+        lambda p: loss.loss(p, *args),
+        lambda p: loss_grad(p, *args),
         zero_function,
         trivial_prox,
         verbose=True,
