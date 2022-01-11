54974f472a8cd3882196adce081e67cb1140417c
zorian15 <zoriant15@gmail.com>
2022-01-11 13:07:31 -0800

    formatting.

--- polyclonal/bootstrap.py
+++ polyclonal/bootstrap.py
@@ -13,7 +13,7 @@ from multiprocessing import Pool
 from itertools import repeat
 
 
-def create_bootstrap_sample(df, seed=0, group_by_col='concentration'):
+def create_bootstrap_sample(df, seed=0, group_by_col="concentration"):
     """ Return a bootstrapped sample of a pandas data frame, maintaining the
     same number of items when grouped by a given column.
 
@@ -34,7 +34,7 @@ def create_bootstrap_sample(df, seed=0, group_by_col='concentration'):
     """
     # Check to make sure group_by_col exists -- raise an error otherwise.
     if group_by_col is not None and group_by_col not in df.columns:
-        raise KeyError(f'{group_by_col} is not in provided data frame.')
+        raise KeyError(f"{group_by_col} is not in provided data frame.")
 
     boot_df = []
 
@@ -51,7 +51,7 @@ def create_bootstrap_sample(df, seed=0, group_by_col='concentration'):
     return pd.concat(boot_df)
 
 
-def create_bootstrap_polyclonal(root_polyclonal, seed=0, groups='concentration'):
+def create_bootstrap_polyclonal(root_polyclonal, seed=0, groups="concentration"):
     """Creates a :class:Polyclonal object from bootstrapped dataset and fits model.
 
     Parameters
@@ -71,10 +71,14 @@ def create_bootstrap_polyclonal(root_polyclonal, seed=0, groups='concentration')
     """
     # Check to see if polyclonal has data_to_fit
     # Polyclonal drops duplicate from data_to_fit by default -- should we  keep this?
-    bootstrap_df = create_bootstrap_sample(df=root_polyclonal.data_to_fit, seed=seed, group_by_col=groups)
-    new_polyclonal = polyclonal.Polyclonal(data_to_fit=bootstrap_df,
-                                           n_epitopes=len(root_polyclonal.epitopes),
-                                           collapse_identical_variants=False)
+    bootstrap_df = create_bootstrap_sample(
+        df=root_polyclonal.data_to_fit, seed=seed, group_by_col=groups
+    )
+    new_polyclonal = polyclonal.Polyclonal(
+        data_to_fit=bootstrap_df,
+        n_epitopes=len(root_polyclonal.epitopes),
+        collapse_identical_variants=False,
+    )
     # Re-initialize this model without the attached dataframe and just wt_activity_df and mut_escape_df from the big model.
     # Return the "slim" model?
     # Maybe create a new method for fitting a polyclonal model and returning the slim version?
@@ -147,11 +151,9 @@ class PolyclonalCollection:
 
     """
 
-    def __init__(self,
-                 root_polyclonal,
-                 n_samples=0,
-                 seed=0,
-                ):
+    def __init__(
+        self, root_polyclonal, n_samples=0, seed=0,
+    ):
         """ See main class docstring and :class:Polyclonal documentation for
         details."""
         # TODO Check to see if the polyclonal object has required args `data_to_fit`
@@ -162,10 +164,10 @@ class PolyclonalCollection:
         if n_samples > 0:
             # Create list of bootstrapped polyclonal objects (don't fit yet)
             with Pool(2) as p:
-                self.models = p.starmap(_create_bootstrap_polyclonal, zip(repeat(root_polyclonal), list(range(n_samples))))
-
-
-
+                self.models = p.starmap(
+                    _create_bootstrap_polyclonal,
+                    zip(repeat(root_polyclonal), list(range(n_samples))),
+                )
 
     def fit_models(self, n_threads=1):
         """ Fits :class:Polyclonal models.
@@ -205,7 +207,6 @@ class PolyclonalCollection:
             # May not need this re-assignment since fit() updates params in place.
             self.models = p.map(_fit_polyclonal_model, self.models)
 
-
     def make_predictions(self, variants_df, n_threads=1):
         """Make predictions on variants for models that have parameters for present
         mutations.
@@ -239,7 +240,6 @@ class PolyclonalCollection:
         with Pool(n_threads) as p:
             pred_dfs = p.starmap(predict, zip(self.models, repeat(variants_df)))
         return pred_dfs
-
 
     def _aggregate_predictions():
         """Aggregate predictions from all eligible models.
