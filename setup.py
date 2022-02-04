b19774f52073d5e39e7fa534ef66409894a0287d
Jared Galloway <jaredgalloway07@gmail.com>
2022-02-04 09:38:41 -0800

    update travis and setup python version to 3.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 if not (sys.version_info[0] == 3 and sys.version_info[1] >= 8):
     raise RuntimeError(
-        "polyclonal requires Python >=3.8.\n"
+        "polyclonal requires Python =3.9.\n"
         f"You are using {sys.version_info[0]}.{sys.version_info[1]}."
     )
 
