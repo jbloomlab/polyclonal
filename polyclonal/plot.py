9abeb65bc1f40b0ae689b0a5f5fd049438773662
zorian15 <zoriant15@gmail.com>
2022-03-11 17:03:52 -0800

    enable custom stat passing for heatmaps.

--- polyclonal/plot.py
+++ polyclonal/plot.py
@@ -298,6 +298,7 @@ def mut_escape_heatmap(
     alphabet,
     epitope_colors,
     epitopes=None,
+    stat='escape',
     all_sites=True,
     all_alphabet=True,
     floor_color_at_zero=True,
@@ -318,6 +319,9 @@ def mut_escape_heatmap(
         Maps each epitope name to its color.
     epitopes : array-like or None
         Make plots for these epitopes. If `None`, use all epitopes.
+    stat : str
+        A string of the statistic present in `mut_escape_df`.
+        Options: ['escape', 'mean', 'median', 'std']
     all_sites : bool
         Plot all sites in range from first to last site even if some
         have no data.
@@ -329,7 +333,7 @@ def mut_escape_heatmap(
         values or if minimum is >0.
     share_heatmap_lims : bool
         If `True`, let all epitopes share the same limits in color scale.
-        If `False`, scale each epitopes colors to the min and max escape
+        If `False`, scale each epitopes colors to the min and max stat
         values for that epitope.
     cell_size : float
         Size of cells in heatmap.
@@ -364,8 +368,8 @@ def mut_escape_heatmap(
     wts = mut_escape_df.set_index("site")["wildtype"].to_dict()
 
     df = (
-        df[["epitope", "site", "mutant", "escape"]]
-        .pivot_table(index=["site", "mutant"], values="escape", columns="epitope")
+        df[["epitope", "site", "mutant", stat]]
+        .pivot_table(index=["site", "mutant"], values=stat, columns="epitope")
         .reset_index()
         .merge(
             pd.DataFrame(
