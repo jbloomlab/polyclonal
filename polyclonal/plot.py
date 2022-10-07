c404ea46863293e208b4094649f81f94f164d8e3
jbloom <jbloom@fredhutch.org>
2022-10-06 21:06:05 -0700

    fix bug in wildtype heatmap plot introduced in 2.3
    In version 2.3, introduced a bug that when a min was specified for a slider range, the heatmap plots didn't show wildtype.
    
    That bug is now fixed.
    
    This becomes version 2.4.

--- polyclonal/plot.py
+++ polyclonal/plot.py
@@ -373,7 +373,10 @@ def lineplot_and_heatmap(
         slider_binding_range_kwargs = {}
     for col, col_kwargs in slider_binding_range_kwargs.items():
         if "min" in col_kwargs:
-            data_df = data_df[data_df[col] >= col_kwargs["min"]]
+            data_df = data_df[
+                (data_df[col] >= col_kwargs["min"])
+                | (data_df["wildtype"] == data_df["mutant"])
+            ]
 
     categories = data_df[category_col].unique().tolist()
     show_category_label = show_single_category_label or (len(categories) > 1)
