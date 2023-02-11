3e91ded93b64585b826d08e30947a83a01f37208
jbloom <jbloom@fredhutch.org>
2023-02-11 06:31:15 -0800

    add 1:1 merge check in mut escape plotting

--- polyclonal/polyclonal_collection.py
+++ polyclonal/polyclonal_collection.py
@@ -660,7 +660,11 @@ class PolyclonalCollection:
             for df in df_to_merge:
                 if not self.sequential_integer_sites and "site" in df.columns:
                     df = df.assign(site=lambda x: x["site"].astype(str))
-                kwargs["data_df"] = kwargs["data_df"].merge(df, how="left")
+                kwargs["data_df"] = kwargs["data_df"].merge(
+                    df,
+                    how="left",
+                    validate="1:1",
+                )
 
         if "category_colors" not in kwargs:
             kwargs["category_colors"] = self.epitope_colors
